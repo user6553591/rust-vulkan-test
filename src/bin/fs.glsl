ebb1c954049a0c37e6b59201f60e069fcb3effbe
Cooper EdenDay <cooperedenday@protonmail.com>
2016-12-15 03:40:46 -0500

    Changed object color to light grey.

--- src/bin/fs.glsl
+++ src/bin/fs.glsl
@@ -19,8 +19,8 @@ const vec3 LIGHT = vec3(0.0, 0.0, 1.0);
 
 void main() {
     float brightness = dot(normalize(v_normal), normalize(LIGHT));
-    vec3 dark_color = vec3(0.6, 0.0, 0.0);
-    vec3 regular_color = vec3(1.0, 0.0, 0.0);
+    vec3 dark_color = vec3(0.6, 0.6, 0.6);
+    vec3 regular_color = vec3(0.8, 0.8, 0.8);
 
     f_color = vec4(mix(dark_color, regular_color, brightness), 1.0);
 }
