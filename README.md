eebfbf6f0068e527cadd8e55383059e407680684
Cooper EdenDay <cooperedenday@protonmail.com>
2017-01-04 08:18:54 -0500

    Added version to build.sh output archive.

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # rust-vulkan-test
 A small program for learning vulkan/vulkano.
 
-The Program Running on My Laptop with a GTX 960M(>3000 FPS)
+The Program Running on My Laptop with a GTX 960M (>3000 FPS)
 ![Screenshot of the program with a rotating monkey](screenshots/2016-12-31.png)
 
 ### Dependencies
 * Cargo
 * GNU Coreutils or equivalent
+* Gawk or equivalent
 * Rsync
 * XZ Utils
--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ cargo update && cargo build --release
 mkdir target/build/
 cp target/release/vulcan-test target/build/
 rsync -av --progress assets target/build/ --exclude assets/build
+version="v$(grep version Cargo.toml | awk '{print $3}' | tr -d '"')"
+filename=vulcan-test-$version-linux-x86_64.tar
 cd target/build/
-tar cf vulcan-test-linux-x86_64.tar * --remove-files
-xz --compress -9e vulcan-test-linux-x86_64.tar --force
+tar cf $filename * --remove-files
+xz --compress -9e $filename --force
