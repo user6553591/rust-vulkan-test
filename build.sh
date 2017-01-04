0ea91f1991bb5dabd80d30288dd6221920197633
Cooper EdenDay <cooperedenday@protonmail.com>
2017-01-04 07:43:44 -0500

    Made build.sh clean up after itself.

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ mkdir target/build/
 cp target/release/vulcan-test target/build/
 rsync -av --progress assets target/build/ --exclude assets/build
 cd target/build/
-tar cf vulcan-test-linux-x86_64.tar *
-xz --compress -9e vulcan-test-linux-x86_64.tar
+tar cf vulcan-test-linux-x86_64.tar * --remove-files
+xz --compress -9e vulcan-test-linux-x86_64.tar --force
