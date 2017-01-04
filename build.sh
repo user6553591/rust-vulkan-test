654d3730b8160a1486da59c1decaaeaa8eff8e5e
Cooper EdenDay <cooperedenday@protonmail.com>
2017-01-04 15:09:30 -0500

    Fixed the fix.

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ rsync -av --progress assets target/build/ --exclude assets/build
 version="v$(grep version Cargo.toml | awk '{print $3}' | tr -d '"')"
 filename=vulcan-test-$version-linux-x86_64.tar
 cd target/build/
-rm -r *
+rm -r *.tar.*
 tar cf $filename * --remove-files
 xz --compress -9e $filename --force
